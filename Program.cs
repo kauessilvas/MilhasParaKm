87208ae5568a975ab5b43e6c9067f839b9ef22ea
kauessilvas <kauesantosds5@gmail.com>
2025-04-05 14:05:10 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+double milhas, quilometros;
+Console.WriteLine("===== Milhas Para Km =====");
+while (true)
+{
+    Console.Write("Digite a medida em milhas: ");
+    string? entrada = Console.ReadLine();
+
+    if (double.TryParse(entrada, out milhas))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Valor inválido! Por favor, digite um número.");
+    }
+}
+Console.WriteLine("==========================");
+
+quilometros = milhas * 1.609;
+Console.WriteLine($"{quilometros} km");
